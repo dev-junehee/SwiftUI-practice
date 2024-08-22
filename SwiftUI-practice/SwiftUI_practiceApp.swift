37a73d3e2d339c071cb09567a57030ed89fa16e2
dev-junehee <dev.junehee@gmail.com>
2024-08-22 10:02:47 +0900

    Initial Commit

---
+++ SwiftUI-practice/SwiftUI_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_practiceApp.swift
+//  SwiftUI-practice
+//
+//  Created by junehee on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
